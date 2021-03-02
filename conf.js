2e99bcd97a2fed73aa377d9290eb1603293b0fcd
Marcos Paulo Barbosa <mpbarbosa_@outlook.com>
2021-03-01 23:39:26 -0300

    Jasmine

--- conf.js
+++ conf.js
@@ -1,4 +1,23 @@
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+
 exports.config = {
+    framework: 'jasmine',
+    jasmineNodeOpts: {
+      showColors: true,
+      silent: true,
+      defaultTimeoutInterval: 360000,
+      print: function () {
+      }
+    },
+    onPrepare: function(){
+        jasmine.getEnv().addReporter(new SpecReporter({
+          displayFailuresSummary: true,
+          displayFailuredSpec: true,
+          displaySuiteNumber: true,
+          displaySpecDuration: true
+        }));
+      },
+    
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['specs/login.spec.js']
 };
--- specs/login.spec.js
+++ specs/login.spec.js
@@ -1,24 +1,41 @@
+
 const {browser} = require('protractor');
 const { isMainThread } = require('worker_threads');
 
 var homePage = require('../pageObjects/homePage.po.js');
 var authenticationPage = require('../pageObjects/authenticationPage.po.js');
 var myAccountPage = require('../pageObjects/myAcccountPage.po.js')
-const {expect} = require('chai');
 
 describe('Login no site de compra: ', function(){
-    beforeAll(function(){
+    beforeEach(function(){
         homePage.visit();
     });
 
     it('Realizar login com sucesso na aplicação', 
-    async function(){
-        await homePage.btnSignIn.click();
+     function(){
+        homePage.visit();
+         homePage.btnSignIn.click();
         authenticationPage.login('automacao2@protractor.com.br','teste123')
-        expect(await myAccountPage.getTextName()).to.be.eq("Protractor Teste");
-    })
+        expect(myAccountPage.getTextName()).toEqual("Protractor Teste");
+    });
+
+    it('Realizar login com sucesso na aplicação2', 
+     function(){
+        expect(myAccountPage.getTextName()).toEqual("Protractor Teste");
+    });
+
+    it('Realizar login com sucesso na aplicação3', 
+     function(){
+        expect(myAccountPage.getTextName()).toEqual("Protractor Teste");
+    });
+
+    it('Realizar login com sucesso na aplicação4', 
+     function(){
+        expect(myAccountPage.getTextName()).toEqual("Protractor Teste");
+    });
+    
 
     afterAll(function(){
         browser.close();
-    })
+    });
 });
